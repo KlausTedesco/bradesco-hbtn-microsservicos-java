64749f7efdf00d9b98b58169b6a41733f40dca19
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-11-27 15:25:56 -0300

    Ajuste

--- 0x02/servicos_API_REST/src/main/java/com/example/demo/Song.java
+++ 0x02/servicos_API_REST/src/main/java/com/example/demo/Song.java
@@ -3,83 +3,86 @@ package com.example.demo;
 import java.util.Objects;
 
 public class Song {
-    private Integer id;
-    private String nome;
-    private String artista;
-    private String album;
-    private String anoLancamento;
+	private Integer id;
+	private String nome;
+	private String artista;
+	private String album;
+	private String anoLancamento;
 
-    public Song() {
-    }
+	public Song() {
+	}
 
-    public Song(Integer id, String nome, String artista, String album, String anoLancamento) {
-        this.id = id;
-        this.nome = nome;
-        this.artista = artista;
-        this.album = album;
-        this.anoLancamento = anoLancamento;
-    }
+	public Song(Integer id, String nome, String artista, String album, String anoLancamento) {
+		this.id = id;
+		this.nome = nome;
+		this.artista = artista;
+		this.album = album;
+		this.anoLancamento = anoLancamento;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getArtista() {
-        return artista;
-    }
+	public String getArtista() {
+		return artista;
+	}
 
-    public void setArtista(String artista) {
-        this.artista = artista;
-    }
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
 
-    public String getAlbum() {
-        return album;
-    }
+	public String getAlbum() {
+		return album;
+	}
 
-    public void setAlbum(String album) {
-        this.album = album;
-    }
+	public void setAlbum(String album) {
+		this.album = album;
+	}
 
-    public String getAnoLancamento() {
-        return anoLancamento;
-    }
+	public String getAnoLancamento() {
+		return anoLancamento;
+	}
 
-    public void setAnoLancamento(String anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
+	public void setAnoLancamento(String anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Song song = (Song) o;
-        return Objects.equals(id, song.id) && Objects.equals(nome, song.nome) && Objects.equals(artista, song.artista) && Objects.equals(album, song.album) && Objects.equals(anoLancamento, song.anoLancamento);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(album, anoLancamento, artista, id, nome);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, artista, album, anoLancamento);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Song other = (Song) obj;
+		return Objects.equals(album, other.album) && Objects.equals(anoLancamento, other.anoLancamento)
+				&& Objects.equals(artista, other.artista) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome);
+	}
 
-    @Override
-    public String toString() {
-        return "Song{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", artista='" + artista + '\'' +
-                ", album='" + album + '\'' +
-                ", anoLancamento='" + anoLancamento + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Song [id=" + id + ", nome=" + nome + ", artista=" + artista + ", album=" + album + ", anoLancamento="
+				+ anoLancamento + "]";
+	}
+
 }
