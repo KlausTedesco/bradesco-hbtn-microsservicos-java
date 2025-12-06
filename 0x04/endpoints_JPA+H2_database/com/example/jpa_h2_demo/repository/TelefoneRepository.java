16adef800c3e296e4ae394abf6da666c3c9742be
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-12-06 15:56:49 -0300

    Definindo Serviços para a API REST usando Spring Data JPA + H2 Database

---
+++ 0x04/endpoints_JPA+H2_database/com/example/jpa_h2_demo/repository/TelefoneRepository.java
@@ -0,0 +1,8 @@
+package com.example.jpa_h2_demo.repository;
+
+import com.example.jpa_h2_demo.model.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelefoneRepository extends JpaRepository<Telefone, Integer> {
+
+}
